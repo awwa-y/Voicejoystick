0ecb5f26014b35a9b52701f6e380b80cb34c9f28
awwa-y <3358735079@qq.com>
2026-04-23 22:47:34 +0800

    Refactor log class to Logger with enhanced features

--- mawinHcom/src/log.h
+++ mawinHcom/src/log.h
@@ -1,12 +1,40 @@
 #ifndef LOG_H
 #define LOG_H
 
-class log
+#include <QString>
+#include <QFile>
+#include <QTextStream>
+#include <QMutex>
+#include <QDateTime>
+
+class Logger
 {
 public:
-    log();
+    enum Level {
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR,
+        FATAL
+    };
+
+    static void init(const QString &filePath = "app.log");
+    static void debug(const QString &msg);
+    static void info(const QString &msg);
+    static void warning(const QString &msg);
+    static void error(const QString &msg);
+    static void fatal(const QString &msg);
+    static void Log(Level level, const QString &msg);
+
 private:
-    int a;
+    static QString levelToString(Level level);
+    static QString currentTimestamp();
+    static void writeToFile(const QString &message);
+
+    static QString m_filePath;
+    static Level m_minLevel;
+    static QMutex m_mutex;
+    static bool m_initialized;
 };
 
-#endif // LOG_H
+#endif
