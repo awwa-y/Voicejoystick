56bec609bd41f9e792e16bb25bb47ee97315a2b1
awwa-y <3358735079@qq.com>
2026-04-23 22:47:10 +0800

    Refactor logging functionality in log.cpp

--- mawinHcom/src/log.cpp
+++ mawinHcom/src/log.cpp
@@ -1,5 +1,91 @@
-#include "log.h"
+#include "Log.h"
+#include <QDebug>
 
-log::log() {
+QString Logger::m_filePath;
+Logger::Level Logger::m_minLevel = DEBUG;
+QMutex Logger::m_mutex;
+bool Logger::m_initialized = false;
 
+void Logger::init(const QString &filePath)
+{
+    QMutexLocker locker(&m_mutex);
+    m_filePath = filePath;
+    m_initialized = true;
+
+    QFile file(m_filePath);
+    if (file.open(QIODevice::WriteOnly | QIODevice::Truncate | QIODevice::Text)) {
+        QTextStream out(&file);
+        out << "=== Application Started at " << QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss") << " ===\n";
+        file.close();
+    }
+}
+
+void Logger::debug(const QString &msg)
+{
+    Log(DEBUG, msg);
+}
+
+void Logger::info(const QString &msg)
+{
+    Log(INFO, msg);
+}
+
+void Logger::warning(const QString &msg)
+{
+    Log(WARNING, msg);
+}
+
+void Logger::error(const QString &msg)
+{
+    Log(ERROR, msg);
+}
+
+void Logger::fatal(const QString &msg)
+{
+    Log(FATAL, msg);
+}
+
+void Logger::Log(Level level, const QString &msg)
+{
+    if (!m_initialized) {
+        qDebug() << msg;
+        return;
+    }
+    QMutexLocker locker(&m_mutex);
+
+    QString message = QString::asprintf("[%s] [%s] %s",
+                                        qPrintable(currentTimestamp()),
+                                        qPrintable(levelToString(level)),
+                                        qPrintable(msg));
+    qDebug().noquote() << message;
+
+    // 输出到文件
+    writeToFile(message);
+}
+
+QString Logger::levelToString(Level level)
+{
+    switch (level) {
+    case DEBUG:   return "DEBUG";
+    case INFO:    return "INFO";
+    case WARNING: return "WARNING";
+    case ERROR:   return "ERROR";
+    case FATAL:   return "FATAL";
+    default:      return "UNKNOWN";
+    }
+}
+
+QString Logger::currentTimestamp()
+{
+    return QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss");
+}
+
+void Logger::writeToFile(const QString &message)
+{
+    QFile file(m_filePath);
+    if (file.open(QIODevice::Append | QIODevice::Text)) {
+        QTextStream out(&file);
+        out << message << "\n";
+        file.close();
+    }
 }
